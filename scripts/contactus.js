0088d4a03550a804e291b651286b792452f24d80
Guest User <Guest@Gareths-MacBook-Pro.local>
2019-06-18 11:39:56 +0100

    final commit

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -1,5 +1,18 @@
 
 
 
-    console.log('Hello');
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let message = document.getElementById("message");
+let submit = document.getElementById("submit");
+function onChange(event) {
+    let valid = firstName.value.length && lastName.value.length && message.value.length;
+    console.log(valid,valid>0);
+    submit.disabled=valid==0;
+}
+firstName.addEventListener("change", onChange);
+submit.disabled = true;
+lastName.addEventListener("change", onChange);
+message.addEventListener("change", onChange);
+
 
